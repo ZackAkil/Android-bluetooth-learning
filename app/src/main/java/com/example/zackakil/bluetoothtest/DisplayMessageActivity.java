f5ad2a2274eb6db7e2dd8bd55b0a574c67ae53f5
ZackAkil <zackakil94@gmail.com>
2016-11-05 22:17:54 +0000

    init commit

---
+++ app/src/main/java/com/example/zackakil/bluetoothtest/DisplayMessageActivity.java
@@ -0,0 +1,62 @@
+package com.example.zackakil.bluetoothtest;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView  display = (TextView) findViewById(R.id.textView2);
+        display.setText(msg);
+
+        doBluetooth();
+
+    }
+
+    private void doBluetooth(){
+
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        }
+
+        ArrayList mArrayAdapter = new ArrayList();
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        // If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mArrayAdapter);
+
+        ListView listView = (ListView) findViewById(R.id.deviceList);
+        listView.setAdapter(adapter);
+
+
+
+
+
+    }
+}
